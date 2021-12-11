37e547d0e275ecb797f777a6b077d818f5b8d05b
ErdemOzgen <m.erdemozgen@gmail.com>
2021-12-12 00:15:54 +0300

    Add Exception Code

--- AdvancedCpp/BasicExceptions/src/BasicExceptions.cpp
+++ AdvancedCpp/BasicExceptions/src/BasicExceptions.cpp
@@ -1,9 +1,9 @@
 //============================================================================
-// Name        : Basic.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Name        : BasicException.cpp
+// Author      : ErdemOzgen
+// Version     :1.0.0
+// Copyright   : MIT License
+// Description : BasicException  C++, Ansi-style
 //============================================================================
 
 #include <iostream>
@@ -14,14 +14,17 @@ void mightGoWrong() {
 
 	bool error1 = true;
 	bool error2 = true;
-
+    bool error3 = true;
 	if(error1) {
-		throw "Something went wrong.";
+		throw "Something went wrong. Const Char * Catching";
 	}
 
 	if(error2) {
-		throw string("Something else went wrong.");
+		throw string("Something else went wrong. String Catching");
 	}
+    if (error3) {
+        throw 12; // int catching
+    }
 
 }
 
@@ -29,22 +32,26 @@ void usesMightGoWrong() {
 	mightGoWrong();
 }
 
+void TryCatchingFunction(void (*erdem)()) { //https://stackoverflow.com/questions/9410/how-do-you-pass-a-function-as-a-parameter-in-c
+    try {
+        erdem();
+    }
+    catch(int e) {
+        cout << "Error code: " << e << endl;
+    }
+    catch(char const * e) {
+        cout << "Error message: " << e << endl;
+    }
+    catch(string &e) {
+        cout << "string error message: " << e << endl;
+    }
+
+}
 
 
 int main() {
 
-	try {
-		usesMightGoWrong();
-	}
-	catch(int e) {
-		cout << "Error code: " << e << endl;
-	}
-	catch(char const * e) {
-		cout << "Error message: " << e << endl;
-	}
-	catch(string &e) {
-		cout << "string error message: " << e << endl;
-	}
+    TryCatchingFunction(mightGoWrong);
 
 	cout << "Still running" << endl;
 
