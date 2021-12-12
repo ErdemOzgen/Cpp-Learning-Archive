56648bc695193bec8ac82f50b6e22574a1c9d289
ErdemOzgen <m.erdemozgen@gmail.com>
2021-12-12 23:04:28 +0300

    Add cpp

--- AdvancedCpp/CatchingSubclassExceptions/src/CatchingSubclassExceptions.cpp
+++ AdvancedCpp/CatchingSubclassExceptions/src/CatchingSubclassExceptions.cpp
@@ -1,11 +1,4 @@
-//============================================================================
-// Name        : Catching.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
+\
 #include <iostream>
 #include <exception>
 using namespace std;
