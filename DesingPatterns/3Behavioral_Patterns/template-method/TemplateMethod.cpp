04bcfa27d42f5e38cac359903ad8798546298b15
ErdemOzgen <m.erdemozgen@gmail.com>
2021-12-19 14:37:08 +0300

    add Last three Desing patterns

---
+++ DesingPatterns/3Behavioral_Patterns/template-method/TemplateMethod.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+/*
+ * AbstractClass
+ * implements a template method defining the skeleton of an algorithm
+ */
+class AbstractClass
+{
+public:
+  virtual ~AbstractClass() {}
+  
+  void templateMethod()
+  {
+    // ...
+    primitiveOperation1();
+    // ...
+    primitiveOperation2();
+    // ...
+  }
+  
+  virtual void primitiveOperation1() = 0;
+  virtual void primitiveOperation2() = 0;
+  // ...
+};
+
+/*
+ * Concrete Class
+ * implements the primitive operations to carry out specific steps
+ * of the algorithm, there may be many Concrete classes, each implementing
+ * the full set of the required operation
+ */
+class ConcreteClass : public AbstractClass
+{
+public:
+  ~ConcreteClass() {}
+  
+  void primitiveOperation1()
+  {
+    std::cout << "Primitive operation 1" << std::endl;
+    // ...
+  }
+  
+  void primitiveOperation2()
+  {
+    std::cout << "Primitive operation 2" << std::endl;
+    // ...
+  }
+  // ...
+};
+
+
+int main()
+{
+  AbstractClass *tm = new ConcreteClass;
+  tm->templateMethod();
+  
+  delete tm;
+  return 0;
+}
---
+++ DesingPatterns/3Behavioral_Patterns/visitor/Visitor.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+
+class Element;
+class ConcreteElementA;
+class ConcreteElementB;
+
+/*
+ * Visitor
+ * declares a Visit operation for each class of ConcreteElement
+ * in the object structure
+ */
+class Visitor
+{
+public:
+  virtual ~Visitor() {}
+  
+  virtual void visitElementA( ConcreteElementA* const element ) = 0;
+  virtual void visitElementB( ConcreteElementB* const element ) = 0;
+  // ...
+};
+
+/*
+ * Concrete Visitors
+ * implement each operation declared by Visitor, which implement
+ * a fragment of the algorithm defined for the corresponding class
+ * of object in the structure
+ */
+class ConcreteVisitor1 : public Visitor
+{
+public:
+  ~ConcreteVisitor1() {}
+  
+  void visitElementA( ConcreteElementA* const )
+  {
+    std::cout << "Concrete Visitor 1: Element A visited." << std::endl;
+  }
+  
+  void visitElementB( ConcreteElementB* const )
+  {
+    std::cout << "Concrete Visitor 1: Element B visited." << std::endl;
+  }
+  // ...
+};
+
+class ConcreteVisitor2 : public Visitor
+{
+public:
+  ~ConcreteVisitor2() {}
+  
+  void visitElementA( ConcreteElementA* const )
+  {
+    std::cout << "Concrete Visitor 2: Element A visited." << std::endl;
+  }
+  
+  void visitElementB( ConcreteElementB* const )
+  {
+    std::cout << "Concrete Visitor 2: Element B visited." << std::endl;
+  }
+  // ...
+};
+
+/*
+ * Element
+ * defines an accept operation that takes a visitor as an argument
+ */
+class Element
+{
+public:
+  virtual ~Element() {}
+  
+  virtual void accept( Visitor &visitor ) = 0;
+  // ...
+};
+
+/*
+ * Concrete Elements
+ * implement an accept operation that takes a visitor as an argument
+ */
+class ConcreteElementA : public Element
+{
+public:
+  ~ConcreteElementA() {}
+  
+  void accept( Visitor &visitor )
+  {
+    visitor.visitElementA( this );
+  }
+  // ...
+};
+
+class ConcreteElementB : public Element
+{
+public:
+  ~ConcreteElementB() {}
+  
+  void accept( Visitor &visitor )
+  {
+    visitor.visitElementB( this );
+  }
+  // ...
+};
+
+
+int main()
+{
+  ConcreteElementA elementA;
+  ConcreteElementB elementB;
+  
+  ConcreteVisitor1 visitor1;
+  ConcreteVisitor2 visitor2;
+  
+  elementA.accept(visitor1);
+  elementA.accept(visitor2);
+  
+  elementB.accept(visitor1);
+  elementB.accept(visitor2);
+  
+  return 0;
+}
