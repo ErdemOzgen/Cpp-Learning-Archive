284837c0aa611db50cfe16a5e71f22fbd9dfe165
ErdemOzgen <m.erdemozgen@gmail.com>
2021-12-19 14:22:29 +0300

    Add Chain of Responsibility

--- DesingPatterns/2Structural_Patterns/proxy/Proxy.cpp
+++ DesingPatterns/2Structural_Patterns/proxy/Proxy.cpp
@@ -1,13 +1,3 @@
-/*
- * C++ Design Patterns: Proxy
- * Author: Jakub Vojvoda [github.com/JakubVojvoda]
- * 2016
- *
- * Source code is licensed under MIT License
- * (for more details see LICENSE)
- *
- */
-
 #include <iostream>
 
 /*
